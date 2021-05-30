0bf835a4ff27414db370831b6c440c4ff8e04ed6
Daniel R Williams <aperium@gmail.com>
2021-05-29 21:37:05 -0400

    added flagged argument for workbook sheet

--- primalscheme/makefastas.R
+++ primalscheme/makefastas.R
@@ -12,7 +12,9 @@ option_list = list(
   make_option(c("-f", "--file"), type="character", default=NULL, 
               help="dataset file name", metavar="character"),
   make_option(c("-o", "--out"), type="character", default="out", 
-              help="output file directory [default= %default]", metavar="character")
+              help="output file directory [default= %default]", metavar="character"),
+  make_option(c("-s", "--sheet"), type="character", default="1", 
+              help="sheet in workbook [default= %default]", metavar="character")
 ); 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
@@ -22,11 +24,6 @@ if (is.null(opt$file)){
   print_help(opt_parser)
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 }
-if (is.null(opt$out)){
-  print_help(opt_parser)
-  stop("At least one argument must be supplied (output directory).n", call.=FALSE)
-}
-
 
 
 
