69a88655ba79c61122ec88e33523118f87715f9f
Daniel R Williams <aperium@gmail.com>
2021-05-30 08:44:24 -0400

    added comments and arguments

--- fluidigm_pool_design/scripts/assessmatricies.R
+++ fluidigm_pool_design/scripts/assessmatricies.R
@@ -1,6 +1,36 @@
+#!/usr/bin/env Rscript
+
+# name: assessmatricies.R
+# author: Daniel R. Williams
+# date: 30 May 2021
+
+# Description:
+# This script extracts coverage data from json log files.
+# 
+# input: directory to search for json files with coverage data.
+# output: coverage data as a csv file.
+# 
+# example command:  Rscript assessmatricies.R -d <in_dir> -o <out_dir>
+# for help:         Rscript assessmatricies.R -h
 
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, magrittr, fs, reshape2)
+pacman::p_load(tidyverse, magrittr, fs, reshape2, optparse)
+
+# Argument options
+option_list = list(
+  make_option(c("-d", "--dir"), type="character", default=NULL, 
+              help="input directory to search for pim files", metavar="character"),
+  make_option(c("-o", "--out"), type="character", default="pools", 
+              help="output directory [default= %default]", metavar="character")
+); 
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+# errormessages for arguments
+if (is.null(opt$dir)){
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
 
 # reqpacks <- c("tidyverse","stringr","magrittr","fs","reshape2")
 # packstoinstall <- setdiff(reqpacks,installed.packages()[,1])
