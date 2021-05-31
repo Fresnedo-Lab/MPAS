423c27071a3fc3059f4857f9209d1170da300402
Daniel R Williams <aperium@gmail.com>
2021-05-30 20:43:49 -0400

    mandatory parameter working

--- primalscheme/runprimalscheme2.sh
+++ primalscheme/runprimalscheme2.sh
@@ -13,8 +13,6 @@
 # example command:  Rscript runprimalscheme2.sh -d <in_dir> -o <out_dir>
 
 # These are determined by PCR requirements. FLuidigm has strict parameters. Illumina MiSeq is less picky.
-AMPMIN=180
-AMPMAX=500
 
 # Default parameters
 unset in_dir
@@ -42,11 +40,23 @@ do
     esac
 done
 
+# Checks for mandatory argument
+if ! [[ -d $1 ]]
+then
+    echo "-d <in_dir> must be included" >&2
+    exit 1
+fi
+
 
 # This fastas named in the list with specified overlaps
-LIST=($(ls fastas | awk 'BEGIN {FS = "."}{ORS = " "} {print $1}'))
+# List of gene/sequence names
+LIST=($(ls $in_dir | awk 'BEGIN {FS = "."}{ORS = " "} {print $1}'))
+
+
 OVERLAPS=(40 45 50 55 60 65 70 75 80 85 90)
-for OVERLAP in ${OVERLAPS[@]}; do mkdir overlap_$OVERLAP; for GENE in ${LIST[@]}; do primalscheme multiplex -a $AMPMIN -a $AMPMAX -n $GENE -t $OVERLAP -o overlap_$OVERLAP/$GENE -f fastas/$GENE.fasta; done; done
+
+
+for OVERLAP in ${OVERLAPS[@]}; do mkdir overlap_$OVERLAP; for GENE in ${LIST[@]}; do primalscheme multiplex -a $ampmin -a $ampmax -n $GENE -t $OVERLAP -o overlap_$OVERLAP/$GENE -f fastas/$GENE.fasta; done; done
 
 exit
 
