a3cb8fbf9a590b1299864a5e6f546bfd7175ecc5
Daniel R Williams <aperium@gmail.com>
2021-05-30 23:00:52 -0400

    no longer hangs. removed debugs

--- fluidigm_pool_design/scripts/fetchprimers.sh
+++ fluidigm_pool_design/scripts/fetchprimers.sh
@@ -45,21 +45,18 @@ fi
 #LIST=(../../primalscheme/overlap_70/*/*.primer.tsv)
 LIST=$(ls ${dir}/*/*.primer.tsv)
 for name in ${LIST[@]}; do
-	(awk 'BEGIN{FS = "\t"}{OFS="\t"}NR>1{print FILENAME,$1,$2,$3,$4,$5,$6}' "${name}") >> "${FILE}.tmp"
+	(awk 'BEGIN{FS = "\t"}{OFS="\t"}NR>1{print FILENAME,$1,$2,$3,$4,$5,$6}' "${name}") >> ${FILE}.tmp
 done
 
-# TODO: this stuff.
-echo "debug 51"
 
 # prepare the column headers for the new file
-(awk 'BEGIN{OFS="\t"}NR<=1{print "gene",$0}' "${LIST[1]}") > "${FILE}"
+set -- $LIST
+(awk 'BEGIN{OFS="\t"}NR<=1{print "gene",$0}' $1) > "${FILE}"
 
-echo "debut 54"
 
 # fill the new file with the primer data and clean up the "gene" and "name" columns
 awk 'BEGIN{FS="\t|/"}{OFS="\t"}{print $5,$7,$8,$9,$10,$11,$12}' "${FILE}.tmp" >> "${FILE}"
 
-echo "debug 57"
 
 # delete the temporary file
 rm "${FILE}.tmp"
