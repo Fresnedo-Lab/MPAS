678b560da7983459b2bf12920e037bda5592e58f
Daniel R Williams <aperium@gmail.com>
2021-05-30 22:29:04 -0400

    minor improvements. Hangs in loop

--- fluidigm_pool_design/scripts/fetchprimers.sh
+++ fluidigm_pool_design/scripts/fetchprimers.sh
@@ -45,14 +45,14 @@ fi
 #LIST=(../../primalscheme/overlap_70/*/*.primer.tsv)
 LIST=$(ls ${dir}/*/*.primer.tsv)
 for name in ${LIST[@]}; do
-	(awk 'BEGIN{FS = "\t"}{OFS="\t"}NR>1{print FILENAME,$1,$2,$3,$4,$5,$6}' $name) >> $FILE.tmp
+	(awk 'BEGIN{FS = "\t"}{OFS="\t"}NR>1{print FILENAME,$1,$2,$3,$4,$5,$6}' $name) >> ${FILE}.tmp
 done
 
 # prepare the column headers for the new file
-(awk 'BEGIN{OFS="\t"}NR<=1{print "gene",$0}' ${LIST[1]}) > $FILE
+(awk 'BEGIN{OFS="\t"}NR<=1{print "gene",$0}' ${LIST[1]}) > ${FILE}
 
 # fill the new file with the primer data and clean up the "gene" and "name" columns
-awk 'BEGIN{FS="\t|/"}{OFS="\t"}{print $5,$7,$8,$9,$10,$11,$12}' $FILE.tmp >> $FILE
+awk 'BEGIN{FS="\t|/"}{OFS="\t"}{print $5,$7,$8,$9,$10,$11,$12}' ${FILE}.tmp >> ${FILE}
 
 # delete the temporary file
-rm $FILE.tmp
+rm ${FILE}.tmp
